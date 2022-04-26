21f1b6a5ed2c5057226ac2968cbfe795d661c528
parkdoyeon <>
2022-04-26 17:51:54 +0900

    Add day one

---
+++ day_one.exs
@@ -0,0 +1,30 @@
+defmodule DayOne do
+  def prepare(path) do
+    Path.expand(path)
+    |> File.read!()
+    |> String.split("\n")
+  end
+
+  defp parse_integer(str) do
+    case Integer.parse(str) do
+      {num, _} -> num
+      _ -> IO.inspect("Something goes wrong!" <> str)
+    end
+  end
+
+  def find_dups(inputs) do
+    inputs
+    |> Stream.cycle()
+    |> Enum.reduce_while([0], fn n, adds ->
+      [head | _] = adds
+      sum = head + parse_integer(n)
+
+      if sum in adds, do: {:halt, sum}, else: {:cont, [sum | adds]}
+    end)
+  end
+end
+
+"assets/day1.txt"
+|> DayOne.prepare()
+|> DayOne.find_dups()
+|> IO.inspect()
