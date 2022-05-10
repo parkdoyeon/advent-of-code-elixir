5289169b23926f7557355e0c11ba33d2e588af6f
parkdoyeon <>
2022-05-11 01:15:00 +0900

    Refactor day six

--- 2018/day_six.exs
+++ 2018/day_six.exs
@@ -1,5 +1,5 @@
 defmodule DaySix do
-  def prepare_locs(path) do
+  defp prepare_locs(path) do
     Path.expand(path)
     |> File.read!()
     |> String.split("\n")
@@ -11,11 +11,9 @@ defmodule DaySix do
     {String.to_integer(x), String.to_integer(y)}
   end
 
-  def prepare_coords(locs) do
-    {min_x, _} = Enum.min_by(locs, & elem(&1, 0))
-    {_, min_y} = Enum.min_by(locs, & elem(&1, 1))
-    {max_x, _} = Enum.max_by(locs, & elem(&1, 0))
-    {_, max_y} = Enum.max_by(locs, & elem(&1, 1))
+  defp prepare_coords(locs) do
+    {{min_x, _}, {max_x, _}} = Enum.min_max_by(locs, & elem(&1, 0))
+    {{_, min_y}, {_, max_y}} = Enum.min_max_by(locs, & elem(&1, 1))
 
     coords = Enum.flat_map(min_x..max_x, fn from_x ->
               Enum.map(min_y..max_y, fn from_y ->
@@ -23,26 +21,81 @@ defmodule DaySix do
               end)
             end)
 
-    {locs, coords}
+    {locs, coords, {min_x, max_x, min_y, max_y}}
   end
 
   defp distance({x1, y1}, {x2, y2}) do
     abs(x1-x2) + abs(y1-y2)
   end
 
-  def calculate_distance({locs, coords}) do
+  defp calculate_distance({locs, coords, _}) do
     Enum.map(coords, fn coord ->
-      locs
-      |> Enum.map(& distance(&1, coord))
-      |> Enum.sum()
+      Enum.reduce(locs, 0, fn loc, acc -> acc + distance(loc, coord) end)
     end)
+  end
+
+  defp find_closest(coord, locs) do
+    Enum.reduce(locs, {nil, nil}, fn loc, {min_loc, min_dist} ->
+      dist = distance(loc, coord)
+      cond do
+        dist == min_dist -> {".", dist}
+        dist < min_dist -> {loc, dist}
+        true -> {min_loc, min_dist}
+      end
+    end)
+  end
+
+  defp mark_area(locs, coords, margins) do
+    Enum.map(coords, fn coord ->
+      {closest_loc, dist} = find_closest(coord, locs)
+      if in_margin?(coord, margins) do
+        {closest_loc, :inf}
+      else
+        {closest_loc, dist}
+      end
+    end)
+  end
+
+  defp reject_infinites(distances) do
+    infinite_areas = distances
+                    |> Enum.filter(& elem(&1, 1) == :inf)
+                    |> Enum.map(& elem(&1, 0))
+                    |> MapSet.new()
+
+    distances
+    |> Enum.reject(& MapSet.member?(infinite_areas, elem(&1, 0)))
+  end
+
+  defp in_margin?(coord, {min_x, max_x, min_y, max_y}) do
+    case coord do
+      {^min_x, _} -> true
+      {^max_x, _} -> true
+      {_, ^min_y} -> true
+      {_, ^max_y} -> true
+      _ -> false
+    end
+  end
+
+  def part1() do
+    {locs, coords, margins} = "./inputs/day6.txt"
+                              |> prepare_locs()
+                              |> prepare_coords()
+
+    mark_area(locs, coords, margins)
+    |> reject_infinites()
+    |> Enum.frequencies_by(& elem(&1, 0))
+    |> Enum.max_by(& elem(&1, 1))
+  end
+
+  def part2() do
+    "./inputs/day6.txt"
+    |> prepare_locs()
+    |> prepare_coords()
+    |> calculate_distance()
+    |> Enum.filter(& &1 < 10000)
+    |> length()
   end
 end
 
-"./inputs/day6.txt"
-|> DaySix.prepare_locs()
-|> DaySix.prepare_coords()
-|> DaySix.calculate_distance()
-|> Enum.filter(& &1 < 10000)
-|> length()
+DaySix.part1()
 |> IO.inspect()
